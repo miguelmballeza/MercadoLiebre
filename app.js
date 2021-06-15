58519e1f10c64a21f6d07470fde34ff6babefd7a
miguelmballeza <miguelballeza53@aragon.unam.mx>
2021-06-14 20:00:32 -0500

    Insertando archivos originales de los videos de PlayGround - Clase 16
    Todos los archivos necesarios para la inicialización de Proyecto con
    Node.js y uso de Express para el levantamiento de un Servidor.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicFolderPath = path.resolve(__dirname, './public'); 
+
+app.use(express.static(publicFolderPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000.'));

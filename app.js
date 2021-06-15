febe88313183df845bbecc820da4fa25028d4fb2
Trxpxrx <trxpxrx@gmail.com>
2021-06-14 18:52:13 -0700

    todo devuelta a la normalidad

--- app.js
+++ app.js
@@ -8,11 +8,5 @@ app.use(express.static(publicFolderPath));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-});
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
-});
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000.'));
